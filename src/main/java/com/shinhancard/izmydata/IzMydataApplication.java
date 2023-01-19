3f2e78b6d13e9d4ea03d7f46d6c70ff5a6a7de51
2horang2 <cmcm1284@naver.com>
2023-01-19 10:19:12 +0900

    project init

---
+++ src/main/java/com/shinhancard/izmydata/IzMydataApplication.java
@@ -0,0 +1,13 @@
+package com.shinhancard.izmydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzMydataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IzMydataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shinhancard/izmydata/IzMydataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhancard.izmydata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IzMydataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
